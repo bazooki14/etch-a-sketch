09845b47768cf81770ff7267ca14efb8b16fb60d
bazooki14 <brandonbouska14@gmail.com>
2022-11-08 23:10:12 -0800

    Create grid of black squares with user input

--- javascript.js
+++ javascript.js
@@ -1,86 +1,49 @@
 let body = document.querySelector('body');
 
+let button = document.createElement('button');
+button.setAttribute('class', 'button');
+button.textContent = 'PRESS TO SET GRID'
+body.appendChild(button);
+
 let grid = document.querySelector('div');
 grid.setAttribute('class', 'grid');
 body.appendChild(grid);
 
-let row1 = document.createElement('div');
-row1.setAttribute('class', 'row1');
-grid.appendChild(row1);
+let gridSize = 0;
 
-let row2 = document.createElement('div');
-row2.setAttribute('class', 'row2');
-grid.appendChild(row2);
+// prompt('How many squares on each side of the grid?')
 
-let row3 = document.createElement('div');
-row3.setAttribute('class', 'row3');
-grid.appendChild(row3);
+button.addEventListener('click', (e) => {
+    createGrid();
+})
 
-let row4 = document.createElement('div');
-row4.setAttribute('class', 'row4');
-grid.appendChild(row4);
+let i;
+let j;
+let row;
+let block;
 
-let block1 = document.createElement('div');
-block1.setAttribute('class', 'block1');
-row1.appendChild(block1);
+function createGrid() {
+    gridSize = prompt('How many squares on each side of the grid?');
+    for (i = 0; i < gridSize; i++) {
+        row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        grid.appendChild(row);
+        for (j = 0; j < gridSize; j++) {
+            block = document.createElement('div');
+            block.setAttribute('class', 'block');
+            row.appendChild(block);
+        }
+    }
+    blocks = document.getElementsByClassName('block');
+    blocks.forEach(addHover())
+}
 
-let block2 = document.createElement('div');
-block2.setAttribute('class', 'block2');
-row1.appendChild(block2);
+function addHover(div) {
+    div.addEventListener('hover', (e) => {
+        etch();
+    })
+}
 
-let block3 = document.createElement('div');
-block3.setAttribute('class', 'block3');
-row1.appendChild(block3);
-
-let block4 = document.createElement('div');
-block4.setAttribute('class', 'block4');
-row1.appendChild(block4);
-
-let block5 = document.createElement('div');
-block5.setAttribute('class', 'block5');
-row2.appendChild(block5);
-
-let block6 = document.createElement('div');
-block6.setAttribute('class', 'block6');
-row2.appendChild(block6);
-
-let block7 = document.createElement('div');
-block7.setAttribute('class', 'block7');
-row2.appendChild(block7);
-
-let block8 = document.createElement('div');
-block8.setAttribute('class', 'block8');
-row2.appendChild(block8);
-
-let block9 = document.createElement('div');
-block9.setAttribute('class', 'block9');
-row3.appendChild(block9);
-
-let block10 = document.createElement('div');
-block10.setAttribute('class', 'block10');
-row3.appendChild(block10);
-
-let block11 = document.createElement('div');
-block11.setAttribute('class', 'block11');
-row3.appendChild(block11);
-
-let block12 = document.createElement('div');
-block12.setAttribute('class', 'block12');
-row3.appendChild(block12);
-
-let block13 = document.createElement('div');
-block13.setAttribute('class', 'block13');
-row4.appendChild(block13);
-
-let block14 = document.createElement('div');
-block14.setAttribute('class', 'block14');
-row4.appendChild(block14);
-
-let block15 = document.createElement('div');
-block15.setAttribute('class', 'block15');
-row4.appendChild(block15);
-
-let block16 = document.createElement('div');
-block16.setAttribute('class', 'block16');
-row4.appendChild(block16);
-
+function etch() {
+    setAttribute('class', 'etched');
+}
